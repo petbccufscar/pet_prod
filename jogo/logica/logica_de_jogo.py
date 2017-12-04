d1ad6e06251fb13e9465e03ec999fe42c54f19f9
renatasarmet <renatassmendes@hotmail.com>
2017-12-04 20:28:47 -0200

    Criacao de metodos de compra e venda de modulos na logica

--- jogo/logica/logica_de_jogo.py
+++ jogo/logica/logica_de_jogo.py
@@ -1,7 +1,25 @@
--*-coding:utf-8-*-
+#-*-coding:utf-8-*-
+from Time import Time
 
 class Logica():
+    times = []
     def __init__(self, qtd_rodadas, nrotimes):
         self.qtd_rodadas = qtd_rodadas
         self.medicos_por_perfil = nrotimes*6
         self.modulos = []
+
+
+    def comprar_modulo(self, id_time, id_modulo):
+        if id_modulo in self.modulos:           # fazer verificação se existe esse módulo
+            self.times[id_time].modulos.append(id_modulo)
+            return True
+        else:
+            return False
+
+    def vender_modulo(self, id_time, id_modulo):
+        if id_modulo in self.times[id_time].modulos:        # fazer verificação se existe o time tem esse módulo
+            i = self.times[id_time].modulos.index(id_modulo)
+            del self.times[id_time].modulos[i]
+            return True
+        else:
+            return False
