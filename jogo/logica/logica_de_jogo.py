2295e69bd9e19c4344e795a2afe3ec16df766627
juliamourac <juliamourac@hotmail.com>
2017-12-04 19:36:28 -0200

    esqueleto da classe lógica

---
+++ jogo/logica/logica_de_jogo.py
@@ -0,0 +1,7 @@
+-*-coding:utf-8-*-
+
+class Logica():
+    def __init__(self, qtd_rodadas, nrotimes):
+        self.qtd_rodadas = qtd_rodadas
+        self.medicos_por_perfil = nrotimes*6
+        self.modulos = []
