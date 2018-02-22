8521d952eda9d040251ed441714ad61f8b874d47
italo <it-jorge@hotmail.com>
2018-02-22 17:54:42 -0300

    aprimoramentos no timer

--- jogo/logica/logica_de_jogo.py
+++ jogo/logica/logica_de_jogo.py
@@ -1,26 +1,46 @@
 import datetime
-from datetime import timedelta
 import _thread
 from time import sleep
-def next():
 
-    return 5000000
 
-def atualiza_timer():
-    global timer
-    anterior = datetime.datetime.utcnow()
-    print (anterior)
-    timer = next()
-    while(True):
-        atual = datetime.datetime.utcnow()
+class Logica(object):
+    def __init__(self, qtd_rodadas, nrotimes):
+        self.qtd_rodadas = qtd_rodadas
+        self.medicos_por_perfil = nrotimes*6
+        self.modulos = []
+        self.rodadas = []
+        self.rodada_atual = 0
 
-        delta_time = atual - anterior
+    def next(self):
+        # TODO: código de fim de rodada
+        # Notificar os clients que acabou a rodada (websockets)
+        # Fazer Calculo das estatisticas
+
+        self.rodada_atual = self.rodada_atual + 1
+        print(self.rodada_atual)
+        if(self.rodada_atual == len(self.rodadas)):
+            # Notificar fim de jogo
+            return 0
+
+        # Notificar os clients que a proxima rodada comecou
+        return self.rodadas[self.rodada_atual].duracao * 60 * 1000000
+
+    def atualiza_timer(self):
+        timer = self.rodadas[0].duracao * 60 * 1000000
         anterior = datetime.datetime.utcnow()
-        timer = timer - delta_time.microseconds - delta_time.seconds*1000000
-        print(timer)
-        sleep(0.1)
-        if(timer < 0):
-            print("zerou")
-            timer = next()
+        print (anterior)
+        while(True):
+            atual = datetime.datetime.utcnow()
+            delta_time = atual - anterior
+            anterior = datetime.datetime.utcnow()
+            timer = timer - delta_time.microseconds - delta_time.seconds*1000000
+            print(timer)
+            sleep(0.1)
+            if(timer < 0):
+                print("zerou")
+                timer = next()
+
+#para teste
+JogoAtual = Logica(5,5)
 def init_timer():
-    _thread.start_new_thread(atualiza_timer, ())
+    _thread.start_new_thread(JogoAtual.atualiza_timer, ())
