a5516abb30a42ff8c746d05a6a4935b261f3ee39
italo <it-jorge@hotmail.com>
2018-04-17 19:35:25 -0300

    Passo 4 iniciado

--- jogo/static/js/tela_pre_jogo.js
+++ jogo/static/js/tela_pre_jogo.js
@@ -1,6 +1,7 @@
 var passoAtual = 0;
 mostraPasso(passoAtual)
 var emprestimos = [];
+var times = [];
 
 function mostraPasso(n) {
   var x = document.getElementsByClassName("passo");
@@ -20,10 +21,10 @@ function mostraPasso(n) {
 function avancarPasso(n){
 
   var x = document.getElementsByClassName("passo");
-  x[passoAtual].style.display = "none";
   if(passoAtual + n >= x.length){
       return;
   }
+  x[passoAtual].style.display = "none";
   passoAtual = passoAtual + n;
   mostraPasso(passoAtual);
 }
@@ -45,4 +46,20 @@ function remover_emprestimo(but){
   linha_selecionada = but.parentElement.parentElement.rowIndex;
   emprestimos.splice(linha_selecionada -1, 1)
   document.getElementById("t_emprestimo").deleteRow(linha_selecionada);
+}
+
+function adiciona_time(){
+  var table = document.getElementById("t_times").getElementsByTagName('tbody')[0];
+  var row = table.insertRow(-1);
+  var input = document.getElementById("in_times");
+  var cell1 = row.insertCell(-1);
+  var cell2 = row.insertCell(-1);
+  cell1.innerHTML = input.value;
+  cell2.setAttribute("style", "text-align:right");
+  cell2.innerHTML = "<button onclick=\"remover_time(this)\" class=\"btn btn-default inline\">x</button>"
+}
+
+function remover_time(but){
+  linha_selecionada = but.parentElement.parentElement.rowIndex;
+  document.getElementById("my_table").deleteRow(linha_selecionada);
 }
