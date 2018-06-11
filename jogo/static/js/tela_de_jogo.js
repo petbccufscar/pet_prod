8aca49a032277946d1b4222304d6fed2c20998f0
Italo Vieira <it-jorge@hotmail.com>
2018-06-11 00:41:09 -0300

    barra lateral modificada

--- jogo/static/js/tela_de_jogo.js
+++ jogo/static/js/tela_de_jogo.js
@@ -29,6 +29,21 @@ socket_rodada.onmessage = function(e) {
 
 }
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
 
 socket_rodada.onopen = function() {
     socket_rodada.send("hello world");
