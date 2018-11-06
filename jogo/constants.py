49169ac13be8a1a129a86287f2e747fa4c87b813
Cassiano Maia <cassmala@gmail.com>
2018-11-06 21:57:58 -0200

    Adcionando arquivo constants.py

---
+++ jogo/constants.py
@@ -0,0 +1,53 @@
+TEMPLATE_JOGO_INDEX = 'jogo/index.html'
+TEMPLATE_JOGO_BASE_CONFIGURACOES = 'jogo/base_configuracoes.html'
+TEMPLATE_JOGO_BASE_APLICAR_DINAMICA = 'jogo/base_aplicar_dinamica.html'
+TEMPLATE_JOGO_NAO_INICIADO = 'jogo/nao_iniciado.html'
+TEMPLATE_JOGO_TELA_DE_JOGO = 'jogo/tela_de_jogo.html'
+TEMPLATE_JOGO_MEU_HOSPITAL = 'jogo/meu_hospital.html'
+TEMPLATE_JOGO_DASHBOARD = 'jogo/dashboard.html'
+TEMPLATE_JOGO_LOGIN = 'jogo/login.html'
+TEMPLATE_JOGO_LOGIN_JOGADOR = 'jogo/login_jogador.html'
+TEMPLATE_JOGO_RANKING = 'jogo/jogo_ranking.html'
+
+TEMPLATE_PREJOGO_1 = 'pre_jogo/tela_pre_jogo_1.html'
+TEMPLATE_PREJOGO_2 = 'pre_jogo/tela_pre_jogo_2.html'
+TEMPLATE_PREJOGO_3 = 'pre_jogo/tela_pre_jogo_3.html'
+TEMPLATE_PREJOGO_4 = 'pre_jogo/tela_pre_jogo_4.html'
+TEMPLATE_PREJOGO_5 = 'pre_jogo/tela_pre_jogo_5.html'
+
+TEMPLATE_MEDICO_INDEX = 'medico/medico_index.html'
+TEMPLATE_MEDICO_NEW = 'medico/medico_new.html'
+TEMPLATE_MEDICO_EDIT = 'medico/medico_edit.html'
+
+TEMPLATE_EVENTO_INDEX = 'evento/evento_index.html'
+TEMPLATE_EVENTO_NEW = 'evento/evento_new.html'
+TEMPLATE_EVENTO_EDIT = 'evento/evento_edit.html'
+
+TEMPLATE_RODADA_INDEX = 'rodada/rodada_index.html'
+TEMPLATE_RODADA_NEW = 'rodada/rodada_new.html'
+TEMPLATE_RODADA_EDIT = 'rodada/rodada_edit.html'
+
+TEMPLATE_AREA_INDEX = 'area/area_index.html'
+TEMPLATE_AREA_NEW = 'area/area_new.html'
+TEMPLATE_AREA_EDIT = 'area/area_edit.html'
+
+TEMPLATE_TIME_INDEX = 'time/time_index.html'
+TEMPLATE_TIME_NEW = 'time/time_new.html'
+TEMPLATE_TIME_EDIT = 'time/time_edit.html'
+
+TEMPLATE_EMPRESTIMO_INDEX = 'emprestimo/emprestimo_index.html'
+TEMPLATE_EMPRESTIMO_NEW = 'emprestimo/emprestimo_new.html'
+TEMPLATE_EMPRESTIMO_EDIT = 'emprestimo/emprestimo_edit.html'
+
+TEMPLATE_CLASSESOCIAL_INDEX = 'classe_social/classe_social_index.html'
+TEMPLATE_CLASSESOCIAL_NEW = 'classe_social/classe_social_new.html'
+TEMPLATE_CLASSESOCIAL_EDIT = 'classe_social/classe_social_edit.html'
+
+TEMPLATE_MODULO_INDEX = 'modulo/modulo_index.html'
+TEMPLATE_MODULO_NEW = 'modulo/modulo_new.html'
+TEMPLATE_MODULO_EDIT = 'modulo/modulo_edit.html'
+
+TEMPLATE_400 = '400.html'
+TEMPLATE_403 = '403.html'
+TEMPLATE_404 = '404.html'
+TEMPLATE_500 = '500.html'
