e07a1998f3dd9b9fb997b0f11a5db58283ce355f
Cassiano Maia <cassmala@gmail.com>
2018-11-06 22:31:36 -0200

    Comentarios no arquivo constans

--- jogo/constants.py
+++ jogo/constants.py
@@ -1,3 +1,10 @@
+#title           :constans.py
+#description     :Arquivo de constantes dos templates html.
+#                 Caso seja necessário trocar algum html, basta trocar
+#                 aqui, garantindo que views.py continue funcionando normalmente.
+#author          :github.com/cassianomaia
+
+#Templates html da pasta jogo
 TEMPLATE_JOGO_INDEX = 'jogo/index.html'
 TEMPLATE_JOGO_BASE_CONFIGURACOES = 'jogo/base_configuracoes.html'
 TEMPLATE_JOGO_BASE_APLICAR_DINAMICA = 'jogo/base_aplicar_dinamica.html'
@@ -9,44 +16,64 @@ TEMPLATE_JOGO_LOGIN = 'jogo/login.html'
 TEMPLATE_JOGO_LOGIN_JOGADOR = 'jogo/login_jogador.html'
 TEMPLATE_JOGO_RANKING = 'jogo/jogo_ranking.html'
 
+
+#Templates html da pasta pre_jogo
 TEMPLATE_PREJOGO_1 = 'pre_jogo/tela_pre_jogo_1.html'
 TEMPLATE_PREJOGO_2 = 'pre_jogo/tela_pre_jogo_2.html'
 TEMPLATE_PREJOGO_3 = 'pre_jogo/tela_pre_jogo_3.html'
 TEMPLATE_PREJOGO_4 = 'pre_jogo/tela_pre_jogo_4.html'
 TEMPLATE_PREJOGO_5 = 'pre_jogo/tela_pre_jogo_5.html'
 
+
+#Templates html da pasta Medico
 TEMPLATE_MEDICO_INDEX = 'medico/medico_index.html'
 TEMPLATE_MEDICO_NEW = 'medico/medico_new.html'
 TEMPLATE_MEDICO_EDIT = 'medico/medico_edit.html'
 
+
+#Templates html da pasta evento
 TEMPLATE_EVENTO_INDEX = 'evento/evento_index.html'
 TEMPLATE_EVENTO_NEW = 'evento/evento_new.html'
 TEMPLATE_EVENTO_EDIT = 'evento/evento_edit.html'
 
+
+#Templates html da pasta rodada
 TEMPLATE_RODADA_INDEX = 'rodada/rodada_index.html'
 TEMPLATE_RODADA_NEW = 'rodada/rodada_new.html'
 TEMPLATE_RODADA_EDIT = 'rodada/rodada_edit.html'
 
+
+#Templates html da pasta area
 TEMPLATE_AREA_INDEX = 'area/area_index.html'
 TEMPLATE_AREA_NEW = 'area/area_new.html'
 TEMPLATE_AREA_EDIT = 'area/area_edit.html'
 
+
+#Templates html da pasta time
 TEMPLATE_TIME_INDEX = 'time/time_index.html'
 TEMPLATE_TIME_NEW = 'time/time_new.html'
 TEMPLATE_TIME_EDIT = 'time/time_edit.html'
 
+
+#Templates html da pasta emprestimo
 TEMPLATE_EMPRESTIMO_INDEX = 'emprestimo/emprestimo_index.html'
 TEMPLATE_EMPRESTIMO_NEW = 'emprestimo/emprestimo_new.html'
 TEMPLATE_EMPRESTIMO_EDIT = 'emprestimo/emprestimo_edit.html'
 
+
+#Templates html da pasta classe_social
 TEMPLATE_CLASSESOCIAL_INDEX = 'classe_social/classe_social_index.html'
 TEMPLATE_CLASSESOCIAL_NEW = 'classe_social/classe_social_new.html'
 TEMPLATE_CLASSESOCIAL_EDIT = 'classe_social/classe_social_edit.html'
 
+
+#Templates html da pasta modulo
 TEMPLATE_MODULO_INDEX = 'modulo/modulo_index.html'
 TEMPLATE_MODULO_NEW = 'modulo/modulo_new.html'
 TEMPLATE_MODULO_EDIT = 'modulo/modulo_edit.html'
 
+
+#Templates html dos erros comuns
 TEMPLATE_400 = '400.html'
 TEMPLATE_403 = '403.html'
 TEMPLATE_404 = '404.html'
